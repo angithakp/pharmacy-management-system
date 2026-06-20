a3cff92f9a0c6da0366b14b992e9bdf05d07a09e
angithakp <angithavalsan@gmail.com>
2026-06-20 16:52:02 +0530

    Fix Gmail SMTP

--- pharmacy_management/pharmacy_management/settings.py
+++ pharmacy_management/pharmacy_management/settings.py
@@ -122,8 +122,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = 'angithavalsan@gmail.com'
+EMAIL_HOST_PASSWORD = 'ewdb ffie rxky bsnl'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
