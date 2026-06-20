c0aa9da422867f50a605f96030a7515c483b252a
angithakp <angithavalsan@gmail.com>
2026-06-20 14:13:13 +0530

    Disable SMTP for deployment

--- pharmacy_management/pharmacy_management/settings.py
+++ pharmacy_management/pharmacy_management/settings.py
@@ -115,7 +115,7 @@ LOGOUT_REDIRECT_URL = '/'
 FAST2SMS_API_KEY = "YOUR_FAST2SMS_API_KEY"
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
