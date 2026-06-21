bdb2feb708c3b158529d2114296326ab6181289a
angithakp <angithavalsan@gmail.com>
2026-06-21 16:10:11 +0530

    Added dotenv and Brevo SMTP

--- pharmacy_management/pharmacy_management/settings.py
+++ pharmacy_management/pharmacy_management/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,10 +130,11 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = "angithavalsan@gmail.com"
+EMAIL_TIMEOUT = 30
 
 print("EMAIL USER =", EMAIL_HOST_USER)
-print("EMAIL PASSWORD =", EMAIL_HOST_PASSWORD)
+
 
 RAZORPAY_KEY_ID = "YOUR_RAZORPAY_KEY_ID"
 RAZORPAY_KEY_SECRET = "YOUR_RAZORPAY_SECRET"
